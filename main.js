11605beacc6d64db5f13e517d22c04777dcee938
Denis Arefyev <163993361+DenisArefyev@users.noreply.github.com>
2024-12-13 11:06:58 +0100

    added readme

--- main.js
+++ main.js
@@ -1,40 +1,39 @@
-// Импорт необходимых модулей из файлов data.js и utils.js
 import { eventsStore } from './js/data.js'
 import { createDomElement } from './js/utils.js'
 import { formatDate } from './js/utils.js'
 
-// Получение доступа к HTML-элементам страницы
+// Accessing HTML Elements on a Page
 const allEventsDiv = document.querySelector('.joinEvents__events-block')
 const eventTypeSelect = document.getElementById('event-type')
 const eventDistanceSelect = document.getElementById('event-distance')
 const eventCategorySelect = document.getElementById('event-category')
 
-// Функция для создания карточек событий
+// Function for creating event cards
 function createEvent(arr) {
     arr.forEach((eventElement) => {
-        // Создание ссылки на событие
+        // Creating a link to an event
         const link = createDomElement({ tag: 'a', className: 'joinEvents__events-card', href: '#' })
         allEventsDiv.append(link)
 
-        // Создание контейнера для изображения события
+        // Creating a container for an event image
         const eventImageContainer = createDomElement({ tag: 'div', className: 'joinEvents__image-container' })
         link.append(eventImageContainer)
 
-        // Создание изображения события
+        // Creating an event image
         const eventImage = createDomElement({ tag: 'img', className: 'joinEvents__image', src: eventElement.image })
         eventImageContainer.append(eventImage)
 
-        // Создание контейнера для информации о событии
+        // Creating a container for event information
         const eventsDescription = createDomElement({ tag: 'div', className: 'joinEvents__card-info' })
         link.append(eventsDescription)
 
-        // Создание элементов с информацией о событии
+        // Creating Event Information Elements
         const eventsDate = createDomElement({ tag: 'p', className: 'joinEvents__date', textValue: formatDate(eventElement.date) })
         const eventsHeader = createDomElement({ tag: 'h3', className: 'joinEvents__title', textValue: eventElement.title })
         const eventsCategory = createDomElement({ tag: 'p', className: 'joinEvents__category', textValue: eventElement.category })
         eventsDescription.append(eventsDate, eventsHeader, eventsCategory)
 
-        // Если событие является онлайн-событием, добавить метку "Online Event"
+        // If the event is an online event, add the "Online Event" tag
         if (eventElement.type === 'online') {
             const onlineEventLabel = createDomElement({
                 tag: 'div',
@@ -45,7 +44,7 @@ function createEvent(arr) {
             eventImageContainer.insertBefore(onlineEventLabel, eventImageContainer.firstChild)
         }
 
-        // Если для события указано количество участников, добавить информацию о них
+        // If the number of participants is specified for the event, add information about them
         if (eventElement.attendees) {
             const eventsAtendees = createDomElement({
                 tag: 'p',
@@ -57,21 +56,21 @@ function createEvent(arr) {
     })
 }
 
-// Функция для очистки всех событий
+// Function to clear all events
 function clearEvents() {
     while (allEventsDiv.firstChild) {
         allEventsDiv.removeChild(allEventsDiv.firstChild)
     }
 }
 
-// Функция для фильтрации событий и их отображения
+// Function for filtering events and displaying them
 function filterEvents(arr) {
-// Получение выбранных значений фильтров
+// Getting selected filter values
     const selectedType = eventTypeSelect.value !== 'any' && eventTypeSelect.value;
     const selectedDistance = eventDistanceSelect.value !== 'any' && eventDistanceSelect.value;
     const selectedCategory = eventCategorySelect.value !== 'any' && eventCategorySelect.value;
 
-    // Фильтрация массива событий в соответствии с выбранными параметрами
+    // Filtering an array of events according to selected parameters
     let filteredArr = arr
     if (selectedType) {
         filteredArr = filteredArr.filter((element) => element.type === selectedType)
@@ -83,15 +82,15 @@ function filterEvents(arr) {
         filteredArr = filteredArr.filter((element) => element.category === selectedCategory)
     }
 
-    // Очистка текущих событий и отображение отфильтрованных событий
+    // Clear current events and display filtered events
     clearEvents()
     createEvent(filteredArr)
 }
 
-// Добавление обработчиков событий на элементы фильтров
+// Adding event handlers to filter elements
 eventTypeSelect.addEventListener('change', () => { filterEvents(eventsStore) })
 eventDistanceSelect.addEventListener('change', () => { filterEvents(eventsStore) })
 eventCategorySelect.addEventListener('change', () => { filterEvents(eventsStore) })
 
-// Отображение всех событий при загрузке страницы
+// Display all events when loading a page
 createEvent(eventsStore)
